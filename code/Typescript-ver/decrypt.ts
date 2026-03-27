2f89f1ba463d4dda0dbd60ee770dc662f6d8c7fd
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-03-28 06:16:21 +0900

    Create decrypt.ts

---
+++ code/Typescript-ver/decrypt.ts
@@ -0,0 +1,42 @@
+import { webcrypto } from "crypto";
+import { EncryptedData } from "./types";
+
+const crypto = webcrypto;
+
+export async function decrypt(encryptedObj: EncryptedData, password: string): Promise<string> {
+  const enc = new TextEncoder();
+  const dec = new TextDecoder();
+
+  const salt = Buffer.from(encryptedObj.salt, "base64");
+  const iv = Buffer.from(encryptedObj.iv, "base64");
+  const data = Buffer.from(encryptedObj.data, "base64");
+
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    enc.encode(password),
+    { name: "PBKDF2" },
+    false,
+    ["deriveKey"]
+  );
+
+  const key = await crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt,
+      iterations: 150000,
+      hash: "SHA-256"
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["decrypt"]
+  );
+
+  const decrypted = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv },
+    key,
+    data
+  );
+
+  return dec.decode(decrypted);
+}
